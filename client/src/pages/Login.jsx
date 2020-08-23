480e264aec94241b0222c1e0cf49b0554997a3f7
Sara <saraesalazar@icloud.com>
2020-08-23 14:15:03 -0400

    Added Login Form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import {
+  MDBContainer,
+  MDBCol,
+  MDBInput,
+  MDBBtn,
+  MDBCard,
+  MDBCardBody
+} from 'mdbreact';
 
 const Login = () => {
   return (
-    <div>
-      <h1>Log in to continue</h1>
-    </div>
+    <MDBContainer className="d-flex justify-content-center m-4">
+      <MDBCol md="6">
+        <MDBCard>
+          <MDBCardBody>
+            <form>
+              <p className="h4 text-center py-4">Login</p>
+              <div className="grey-text">
+                <MDBInput
+                  label="Your email"
+                  icon="envelope"
+                  group
+                  type="email"
+                  validate
+                  error="wrong"
+                  success="right"
+                />
+                <MDBInput
+                  label="Your password"
+                  icon="lock"
+                  group
+                  type="password"
+                  validate
+                />
+              </div>
+              <div className="text-center py-4 mt-3">
+                <MDBBtn gradient="blue" type="submit">
+                  Login
+                </MDBBtn>
+              </div>
+            </form>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+    </MDBContainer>
   );
 };
 
