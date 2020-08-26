1106a0819b5d0d2d9eb84122321cdf395cc7c7c1
Cal <calvin.m71@hotmail.com>
2020-08-25 22:29:23 -0400

    Google passport

--- server/routes/open/index.js
+++ server/routes/open/index.js
@@ -1,7 +1,53 @@
 const router = require('express').Router(),
   { sendWelcomeEmail, forgotPasswordEmail } = require('../../emails/index'),
   User = require('../../db/models/user'),
-  bcrypt = require('bcryptjs');
+  GoogleStrategy = require('passport-google-oauth20').Strategy,
+  passport = require('passport');
+bcrypt = require('bcryptjs');
+
+//Google sign in
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/users/auth/google/users'
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log('In index.js line 27');
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
+
+router.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }),
+  (req, res) => {
+    console.log('In Index');
+    res.redirect('/users');
+  }
+);
+
+router.get(
+  '/auth/google/users',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+    res.redirect('/users');
+  }
+);
 
 // Login a user
 router.post('/api/users/login', async (req, res) => {
