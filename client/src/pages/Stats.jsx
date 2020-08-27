4e4dcb87578371d9df80a576828cd040c6a6cabb
Cal <calvin.m71@hotmail.com>
2020-08-27 16:27:44 -0400

    empty chart added

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,44 +1,64 @@
 import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
-import { fetchTask } from '../api';
 
-const Stats = ({ data: { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday } }) => {
-    const [ fetchTask, setFetchTask ] = useState([]);
+// import { fetchTask } from '../api';
 
-    useEffect(() => {
-      const fetchAPI = async () => {
-        setFetchTask( await fetchTask())
-      }
-      fetchAPI();
-    }, [])
+const Stats = () => {
+    const [ lineChart, setLineChart ] = useState({});
 
-    const lineChart = fetchTask.length ? ( 
-    <Line
-      data={{
-        labels: "Your Progress!",
+    const graph = () => {
+      setLineChart({
+        labels: [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ],
         datasets: [
           {
-            data: [ Monday.value, Tuesday.value, Wednesday.value, Thursday.value, Friday.value, Saturday.value, Sunday.value ],
-            label: " ",
-            borderColor: "#3333ff",
-            fill: true,
+            label: "Your Progress!",
+            data: 'insert session hours',
+            backgroundColor: [
+              'rgba( 75, 192, 192, 0.6'
+            ],
+            borderWidth: 4
           }
-        ],
-      }}
-    />
-  ) : null;
+        ]
+      })
+    }
+
+    useEffect(() => {
+      graph()
+    }, [])
 
   return (
     <div style={{ position: 'relative', width: 600, height: 550 }}>
       <h1>You've been productive lately!</h1>
       <h3>Heres your progress!</h3>
+      <div className='d-flex, justify-content-center, width: 85%'>
       <Line
-        options={{
-          responsive: true
-        }}
-        data={{Stats}}
-      />
-       <div>{lineChart}</div>
+          data={lineChart} option={{
+            responsive: true,
+            title: {text: 'Time spent', display: true},
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    autoSkip: true,
+                    maxTicksLimit: 10,
+                    beginAtZero: true
+                  },
+                  gridLines: {
+                    display: false
+                  }
+                }
+              ],
+              xAxes: [
+                {
+                  gridLines: {
+                    display: false
+                  }
+                }
+              ]
+            }
+          }}
+        />
+      </div>
     </div>
   );
 };
