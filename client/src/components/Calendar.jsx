e8dcf9012b5ea1e53f96052c63e68f715db73a4f
Rodrigo Poma <rodpoma@gmail.com>
2020-08-27 12:45:16 -0600

    events can be added, but not yet saved to database

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -6,6 +6,36 @@ import interactionPlugin from '@fullcalendar/interaction';
 import { MDBContainer } from 'mdbreact';
 
 const Calendar = () => {
+  const [eventID, setEventID] = useState(1);
+
+  const handleDateSelect = (selectInfo) => {
+    let title = prompt('Please enter a new title for your event');
+    let calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: eventID,
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      });
+      setEventID(eventID + 1);
+    }
+  };
+
+  const handleEventClick = (clickInfo) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
+      clickInfo.event.remove();
+    }
+  };
+
   return (
     <MDBContainer className="mt-5">
       <FullCalendar
@@ -20,9 +50,13 @@ const Calendar = () => {
         selectable={true}
         selectMirror={true}
         dayMaxEvents={true}
-        // select={handleDateSelect}
-        // eventClick={handleEventClick}
+        select={handleDateSelect}
+        eventClick={handleEventClick}
         // eventsSet={handleEvents}
+        //-- these will communicate with Database
+        // eventAdd={function () {}}
+        // eventChange={function () {}}
+        // eventRemove={function () {}}
       />
     </MDBContainer>
   );
