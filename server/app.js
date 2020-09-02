424c9f10dc5d2e380efb9446d04a035af66c48fc
Cal <calvin.m71@hotmail.com>
2020-09-02 14:15:20 -0400

    commit on "update-chart"

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ const express = require('express'),
   userRouter = require('./routes/secure/users'),
   taskRouter = require('./routes/secure/tasks'),
   sessionRouter = require('./routes/secure/sessions'),
-  chartRouter = require('./routes/secure/charts'),
   cors = require('cors');
 app = express();
 
@@ -36,7 +35,6 @@ app.use(
 app.use(userRouter);
 app.use(taskRouter);
 app.use(sessionRouter);
-app.use(chartRouter);
 
 if (process.env.NODE_ENV === 'production') {
   // Handle React routing, return all requests to React app
